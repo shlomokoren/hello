f497e4068e573771ef515bf361be6ee4d920790c
KimmyLeo <kenpusney@gmail.com>
2015-11-25 19:02:24 +0800

    [#4] fix build

--- src/test/java/net/kimleo/hello/message/ConcreteTokenVisitorTest.java
+++ src/test/java/net/kimleo/hello/message/ConcreteTokenVisitorTest.java
@@ -3,24 +3,30 @@ package net.kimleo.hello.message;
 import net.kimleo.hello.validate.Validator;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.io.PrintStream;
 
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ConcreteTokenVisitorTest {
 
     public static final String HELLO_TOKEN = "hello";
+    @InjectMocks
     private ConcreteTokenVisitor visitor;
     private PrintStream stream;
+    @Mock
     private Validator<String> validator;
 
     @Before
     public void setUp() throws Exception {
         stream = mock(PrintStream.class);
-        validator = mock(Validator.class);
-        visitor = new ConcreteTokenVisitor();
         when(validator.validate(anyString())).thenReturn(true);
     }
 
