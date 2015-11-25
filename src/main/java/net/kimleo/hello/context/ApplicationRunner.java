bccd1bf4b6eab1f0f09d3d35607d54222e1dc6fc
KimmyLeo <kenpusney@gmail.com>
2015-11-25 14:47:39 +0800

    [#004] extract class lookup method to ClassUtils

--- src/main/java/net/kimleo/hello/context/ApplicationRunner.java
+++ src/main/java/net/kimleo/hello/context/ApplicationRunner.java
@@ -4,14 +4,9 @@ import net.kimleo.hello.annotation.Application;
 import net.kimleo.hello.annotation.Component;
 import net.kimleo.hello.annotation.Factory;
 
-import java.io.File;
-import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -22,7 +17,7 @@ public class ApplicationRunner {
 
     public void run(Class<?> appClass) throws Exception {
         assert (appClass.getAnnotation(Application.class) != null);
-        Class[] classes = getClasses(appClass.getPackage().getName());
+        Class[] classes = ClassUtils.getClasses(appClass.getPackage().getName());
         for (Class aClass : classes) {
             addToComponentMappings(aClass);
         }
@@ -57,15 +52,15 @@ public class ApplicationRunner {
     private void addToContextIfItIsAComponent(Class clz)
             throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         if (!clz.isInterface()) {
-            context.put(clz, createInstance(clz));
+            context.put(clz, getInstance(clz));
         }
     }
 
-    private Object createInstance(Class clz)
+    public <T> T getInstance(Class<? extends T> clz)
             throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        if (context.containsKey(clz)) return context.get(clz);
+        if (context.containsKey(clz)) return (T) context.get(clz);
         if (clz.getAnnotation(Factory.class) != null) {
-            return clz.getDeclaredMethod("getInstance").invoke(clz);
+            return (T) clz.getDeclaredMethod("getInstance").invoke(clz);
         }
         Object result = null;
         Constructor[] constructors = clz.getDeclaredConstructors();
@@ -78,40 +73,7 @@ public class ApplicationRunner {
             }
             result = ctor.newInstance(objects.toArray());
         }
-        return result;
+        return (T) result;
     }
 
-    private Class[] getClasses(String packageName)
-            throws ClassNotFoundException, IOException {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        assert classLoader != null;
-        String path = packageName.replace('.', '/');
-        Enumeration<URL> resources = classLoader.getResources(path);
-        List<File> dirs = new ArrayList<>();
-        while (resources.hasMoreElements()) {
-            URL resource = resources.nextElement();
-            dirs.add(new File(resource.getFile()));
-        }
-        ArrayList<Class> classes = new ArrayList<>();
-        for (File directory : dirs) {
-            classes.addAll(findClasses(directory, packageName));
-        }
-        return classes.toArray(new Class[classes.size()]);
-    }
-
-    private List<Class> findClasses(File directory, String packageName) throws ClassNotFoundException {
-        List<Class> classes = new ArrayList<>();
-        File[] files = directory.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                if (file.isDirectory()) {
-                    assert !file.getName().contains(".");
-                    classes.addAll(findClasses(file, packageName + "." + file.getName()));
-                } else if (file.getName().endsWith(".class")) {
-                    classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
-                }
-            }
-        }
-        return classes;
-    }
 }
